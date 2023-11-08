403ba1a4dffebece1c1d587d1dc63243a15b7870
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-08 02:21:24 +0000

    Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from typing import List, TypeVar
 from flask import request
+from itsdangerous import exc
 
 
 class Auth:
@@ -10,7 +11,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if a route path requires auth"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path if path[-1] == "/" else path + "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
