2947b6b5f74de8981ec0620faf772f7158f3555c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-08 03:05:52 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """API Basic Authentication"""
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """Extract Base 64 authorization header"""
+        if (
+            authorization_header is None
+            or not isinstance(authorization_header, str)
+            or authorization_header[:6] != "Basic "
+        ):
+            return None
+        else:
+            return authorization_header[6:]
