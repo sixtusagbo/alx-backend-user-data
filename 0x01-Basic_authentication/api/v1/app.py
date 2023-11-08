842a4543343c86752fc6323991a8299a8a2c4241
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-08 02:50:01 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,15 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE"):
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -32,6 +36,20 @@ def forbidden(error) -> str:
     A user is authenticated but not is not allowed to access the resource.
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Handle before request actions"""
+    if auth is not None:
+        if auth.require_auth(
+            request.path,
+            ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
+        ):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            elif auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from itsdangerous import exc
 
 
 class Auth:
-    """API Auth class"""
+    """API Authentication class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if a route path requires auth"""
@@ -20,7 +20,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
-        return None
+        if request is None or request.headers.get("Authorization") is None:
+            return None
+        else:
+            return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Return current user for a request"""
