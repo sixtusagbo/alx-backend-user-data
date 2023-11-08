8d6f6332c5c31f21a0d7f501f78df4530b6bcdf2
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-08 03:18:14 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """
 Basic Authentication module for the API
 """
+import base64
 from api.v1.auth.auth import Auth
+import binascii
 
 
 class BasicAuth(Auth):
@@ -20,3 +22,17 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Return the decoded value of a base64 string"""
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+        try:
+            base64_bytes = base64.b64decode(base64_authorization_header)
+            return base64_bytes.decode("utf-8")
+        except binascii.Error as e:
+            return None
