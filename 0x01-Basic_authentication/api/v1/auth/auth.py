27dee2b9d2e362e2f914a61f20d7771842469e84
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-08 01:53:32 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """API Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if a route path requires auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Return current user for a request"""
+        return None
