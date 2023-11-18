dd805b37fc4a01a06b091789ac5af986006508dc
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-18 04:48:07 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
+from os import abort
 from flask import Flask, jsonify, request
 from auth import Auth
 
@@ -25,6 +26,23 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Log a user into the app"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        if AUTH.valid_login(email, password):
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", AUTH.create_session(email))
+            return response
+        else:
+            abort(401)
+    except Exception:
+        abort(401)
 
 
 if __name__ == "__main__":
